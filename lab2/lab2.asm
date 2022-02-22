2fc39c4f5a753b258c3471c3334d1bebcd15bc20
Elh-Ayoub <ayoub1998elh@gmail.com>
2022-02-22 14:51:01 +0200

    Update lab2, added 2 edited versions for .exe file

--- lab2/lab2.asm
+++ lab2/lab2.asm
@@ -1,54 +1,36 @@
 include win64a.inc
 
 .data
-	cWritten dq 0 ;
-	Msg db "Calculate the equation 18c/2b  :",0dh,0ah,0 ;
-	input_c db "Value of c:  8",0dh,0ah,0 ;
-	input_b db "Value of b:  3",0dh,0ah,0 ;
-	stdout dq 0
-	titl2 db "Result",0
+	titl2 db "Result of the equation",0
 	buf1 db 1024 dup (0)
-	buf2 db 1024 dup (0)
-	buf3 db 1024 dup (0)
-	result_temp db "Result :  %d ",0dh,0ah,0
-	ttl db "Elementary console password entry program",0
-	cRead dq ?
-	cRead1 dq ?
-	buf0 dq ?
-	buf4 dq ?
-	c1 dd 8
-	b dd 3
+	result_temp db "Calculate the equation (18c/2b) :	%d ",0dh,0ah,
+	"Time (Ticks):	%d ",0dh,0ah,0
+	_c dq 2
+	b dq 4
 .code
 WinMain proc
 	sub rsp,28h
 	mov rbp,rsp
-	invoke GetStdHandle,STD_OUTPUT_HANDLE
-	mov stdout,rax
-	invoke WriteConsole,stdout,ADDR Msg,sizeof Msg,ADDR cWritten,0
-	invoke WriteConsole,stdout,ADDR input_b,sizeof input_b,ADDR cWritten,0
-	invoke WriteConsole,stdout,ADDR input_c,sizeof input_c,ADDR cWritten,0
-
 	;;;;;;;;;;;;;;;;;;;;;;
+	rdtsc			;set current timestamp into r15
+	xchg rax, r15
 	;Calculate equation
-	mov eax, c1
-	mov ecx, 18
-	mul ecx
-	mov ecx, eax
+	mov rax, _c
+	mov rbx, 18
+	mul rbx
 	
-	mov eax, 2
-	mov ebx, b
-	mul ebx
-	mov ebx, eax
+	mov rbx, b
+	shl rbx, 1
+	div rbx
 	
-	mov eax, ecx
-	div ebx
-	
-	invoke wsprintf, ADDR buf2, ADDR result_temp, eax
-	invoke WriteConsole,stdout,ADDR buf2,sizeof buf2,ADDR cWritten,0
-	invoke MessageBox,0,addr buf2,addr titl2,MB_ICONINFORMATION
+	mov r9, rax
+	rdtsc 			 ;Substracting previous timestamp with the current give us the execution time
+	sub rax,r15
+	invoke wsprintf, ADDR buf1, ADDR result_temp, r9, rax
+	invoke MessageBox,0,addr buf1,addr titl2,MB_ICONINFORMATION
 
 	;e:
-	;invoke ExitProcess,0 ;invoke RtlExitUserProcess,0 ;ExitProcess,0
+	invoke ExitProcess,0 ;invoke RtlExitUserProcess,0 ;ExitProcess,0
 	
 WinMain endp
 end
